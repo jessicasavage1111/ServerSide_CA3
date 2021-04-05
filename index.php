5d18fe82968dba59bcf78e242747ef9994a329d0
Jessica Savage <JessicaSavage1111@Hotmail.com>
2021-04-05 19:16:26 +0100

    fixed index

--- index.php
+++ index.php
@@ -96,12 +96,7 @@ value="<?php echo $item['foodID']; ?>">
 value="<?php echo $item['categoryID']; ?>">
 <input id="blue-button" type="submit" value="Edit">
 </form></td>
-<<<<<<< HEAD
-
-<td ><form action="buy_form.php" method="post"
-=======
 <td><form action="buy_form.php" method="post"
->>>>>>> contact-form
 id="delete_record_form">
 <input type="hidden" name="food_id"
 value="<?php echo $item['foodID']; ?>">
