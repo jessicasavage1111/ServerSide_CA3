ad37b7d7aa8546b4452847dbea28102de8e8065e
Jessica Savage <JessicaSavage1111@Hotmail.com>
2021-04-10 22:51:06 +0100

    creating nav bar

--- includes/header.php
+++ includes/header.php
@@ -8,3 +8,12 @@
 <!-- the body section -->
 <body>
 <center><header><h1>The Savage Food Shop</h1></header></center>
+
+<header>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+</header>
