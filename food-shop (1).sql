9c6c26aa1cb2532ce87ccc86aac9725c012e23fe
Jessica Savage <JessicaSavage1111@Hotmail.com>
2021-04-18 22:04:14 +0100

    Added SQL file

--- food-shop (1).sql
+++ food-shop (1).sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2021 at 10:10 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Generation Time: Apr 18, 2021 at 10:59 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -47,6 +46,49 @@ INSERT INTO `categories` (`categoryID`, `categoryName`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact` (
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `reason` text NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`name`, `email`, `date`, `reason`, `message`) VALUES
+('Jessica Savage', 'JessicaSavage1111@Hotmail.com', '2021-04-15', 'Late Delivery', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deliveries`
+--
+
+DROP TABLE IF EXISTS `deliveries`;
+CREATE TABLE `deliveries` (
+  `deliveryID` int(11) NOT NULL,
+  `progress` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `deliveries`
+--
+
+INSERT INTO `deliveries` (`deliveryID`, `progress`) VALUES
+(1, 'Delivered'),
+(2, 'Dispatched'),
+(3, 'Order Processing');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `food`
 --
 
@@ -79,7 +121,7 @@ INSERT INTO `food` (`foodID`, `categoryID`, `code`, `name`, `expiryDate`, `price
 (31, 3, '32631', 'Whole Milk', '2021-03-10', '2.68', '32631.jpg'),
 (32, 3, '39104', 'Low Fat Milk', '2021-03-14', '2.99', '39104.webp'),
 (33, 3, '51764', 'Yogurt', '2021-03-13', '3.00', '51764.jpg'),
-(34, 3, '11182', 'Haagen-Dazs Vanilla Ice Cream', '2021-03-18', '0.00', '11182.jpg'),
+(34, 3, '11182', 'Haagen-Dazs Vanilla Ice Cream', '2021-03-18', '4.50', '11182.jpg'),
 (35, 3, '76134', 'Shredded Chedder Cheese', '2021-03-17', '2.48', '76134.jpg'),
 (36, 4, '53073', 'Brennan\'s Bread', '2021-03-10', '1.58', '53073.jpg'),
 (37, 4, '76864', 'Mini Chocolate Chip Muffins', '2021-03-20', '3.74', '76864.jpg'),
@@ -89,17 +131,49 @@ INSERT INTO `food` (`foodID`, `categoryID`, `code`, `name`, `expiryDate`, `price
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `orderID` int(11) NOT NULL,
+  `foodID` int(11) NOT NULL,
+  `deliveryID` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `foodName` varchar(255) NOT NULL,
   `number` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`orderID`, `foodID`, `deliveryID`, `name`, `address`, `email`, `number`) VALUES
+(1, 20, 1, 'Jessica Savage', '4 Ballygarth Manor', 'JessicaSavage1111@Hotmail.com', 2),
+(2, 33, 2, 'Emma Savage', '4 Ballygarth Manor', 'EmmaSavage1111@Hotmail.com', 1),
+(3, 38, 3, 'Sarah Savage', '17 Grange Rath', 'Sarah@hotmail.com', 4),
+(4, 26, 2, 'Laura Savage', '12 Shire Ridge', 'Laura1111@Hotmail.com', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(6, 'D00227023', '$2y$12$ecvAolMmMo.yl.7U7W4wy.kMo2NJkrBcZd9IkjM.fiUfiWQhFYwEa');
 
 --
 -- Indexes for dumped tables
@@ -112,11 +186,32 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`categoryID`);
 
 --
+-- Indexes for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  ADD PRIMARY KEY (`deliveryID`);
+
+--
 -- Indexes for table `food`
 --
 ALTER TABLE `food`
   ADD PRIMARY KEY (`foodID`),
   ADD UNIQUE KEY `productCode` (`code`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `deliveryID` (`deliveryID`),
+  ADD KEY `foodID` (`foodID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -132,7 +227,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `foodID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `foodID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
